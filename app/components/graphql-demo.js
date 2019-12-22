49cac3e287c95e899ba3a1b4a380d87a18ab4401
Dustin Masters <dustin.masters@olo.com>
2019-12-22 06:13:12 +0000

    Move to ember-apollo

--- app/components/graphql-demo.js
+++ app/components/graphql-demo.js
@@ -1,60 +1,20 @@
 import Component from "@ember/component";
-import { createClient, createRequest } from "urql/core";
-import gql from "graphql-tag";
-import { pipe, subscribe } from "wonka";
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import searchartist from 'my-app/queries/searchartist';
 
 export default class GraphqlDemoComponent extends Component {
+  @service
+  apollo;
+
   didInsertElement() {
     this.fetchData();
   }
+
   fetchData() {
-    const client = createClient({
-      url: "https://graphbrainz.herokuapp.com/graphql"
-    });
-
-    const query = gql`
-      query SearchArtist($search: String!) {
-        search {
-          artists(query: $search, first: 1) {
-            edges {
-              node {
-                name
-                country
-                releases(first: 10) {
-                  edges {
-                    node {
-                      title
-                      date
-                      media {
-                        format
-                        trackCount
-                        tracks {
-                          title
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `;
-
-    const request = createRequest(query, { search: "Courtney Barnett" });
-
-    pipe(
-      client.executeQuery(request),
-      subscribe(({ data, error }) => {
-        if (error) {
-          this.result = JSON.stringify(error);
-        } else {
-          this.result = JSON.stringify(data);
-        }
-      })
-    );
+    this.apollo.query({ query: searchartist, variables: { search: "Courtney Barnett" } }).then(graphqlResult => {
+      this.result = JSON.stringify(graphqlResult);
+    })
   }
 
   @tracked
